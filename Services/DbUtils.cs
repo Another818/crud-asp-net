05f0c8b3c3f0d1e2bdef72ce76d9258d4d6cd9b7
Tomás Menna <pablotomasmenna@gmail.com>
2023-07-14 22:36:36 -0300

    Conexión a Base de datos

---
+++ Services/DbUtils.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplicationSistemaDeReclamo.Services
+{
+    public class DbUtils
+    {
+        public static SqlConnection RecuperarConnection()
+        {
+            //Leer el archivo appsettings.json
+            var builder = new ConfigurationBuilder()
+                   .AddJsonFile($"appsettings.json", true, true);
+
+            var config = builder.Build();
+
+            //Conexión SQL Server
+            string connectionString = config.GetConnectionString("DefaultConnection");
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            return con;
+        }
+    }
+}
--- Services/ReclamosService.cs
+++ Services/ReclamosService.cs
@@ -1,11 +1,23 @@
-﻿using WebApplicationSistemaDeReclamo.Models;
+﻿using System.Data.SqlClient;
+using WebApplicationSistemaDeReclamo.Models;
 namespace WebApplicationSistemaDeReclamo.Services
 {
     public class ReclamosService
     {
         public void AltaReclamos(Reclamo reclamo)
         {
+            SqlConnection sqlConnection = DbUtils.RecuperarConnection();
             //TODO: FALTA EL ALTA EN LA BASE DE DATOS....
+            SqlCommand command = sqlConnection.CreateCommand();
+            command.CommandText = "INSERT INTO reclamos (titulo, descripcion, estado, fechaAlta) VALUES" 
+                + " (@titulo, @descripcion, @estado, @fechaAlta);";
+            command.Parameters.AddWithValue("@titulo", reclamo.Titulo);
+            command.Parameters.AddWithValue("@descripcion", reclamo.Descripcion);
+            command.Parameters.AddWithValue("@estado", reclamo.Estado);
+            command.Parameters.AddWithValue("@fechaAlta", reclamo.FechaAlta);
+            command.ExecuteNonQuery();
+
+            sqlConnection.Close();
         }
 
         public List<Reclamo> RecuperarListadoDeReclamo()
